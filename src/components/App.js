274173d84a609f2bd4959c9ef5b21b559218694c
Your Name <hekstr@gmail.com>
2019-10-29 15:53:49 +0100

    Fix

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import { Route, Switch } from "react-router-dom";
 import Header from "./common/Header";
 import HomePage from "./home/HomePage";
 import GodsPage from "./gods/GodsPage";
-import "./App.css";
 
 function App() {
   return (
