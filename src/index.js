000b4261e99af6704691aa149985c0c3b2641378
Your Name <hekstr@gmail.com>
2019-10-29 15:37:44 +0100

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+// import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
