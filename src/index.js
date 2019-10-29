99b0dd80b634531718548249f89fff006e3aa890
Your Name <hekstr@gmail.com>
2019-10-29 15:40:58 +0100

    Enable scss

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
-// import "./index.css";
+import "./index.scss";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
